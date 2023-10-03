c9a69670186a770b22018487d52618f384bec11a
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-10-03 23:49:38 +0530

    update the api code

--- lib/controller/api/blogs_api_controller.dart
+++ lib/controller/api/blogs_api_controller.dart
@@ -1,27 +1,38 @@
-import 'dart:developer' as developer;
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void fetchBlogs() async {
-  const String url = 'https://intent-kit-16.hasura.app/api/rest/blogs';
-  const String adminSecret =
+import '../../model/blog_model/blog_model.dart';
+
+class APIController extends ChangeNotifier {
+  static const baseUrl = 'https://intent-kit-16.hasura.app/api/rest/blogs';
+  static const adminSecret =
       '32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6';
 
-  try {
-    final response = await http.get(Uri.parse(url), headers: {
-      'x-hasura-admin-secret': adminSecret,
-    });
+  static const headers = {
+    'Content-Type': 'application/json',
+    'x-hasura-admin-secret': adminSecret,
+  };
 
-    if (response.statusCode == 200) {
-      // Request successful, handle the response data here
-      developer.log('Response data: ${response.body}');
-    } else {
-      // Request failed
-      developer.log('Request failed with status code: ${response.statusCode}');
-      developer.log('Response data: ${response.body}');
+  bool isLoading = true;
+  String errorMessage = "";
+
+  BlogModel blogModel = BlogModel(blogs: []);
+
+  Future<void> fetchBlogs() async {
+    try {
+      final response = await http.get(Uri.parse(baseUrl), headers: headers);
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body) as Map<String, dynamic>;
+        blogModel = BlogModel.fromMap(data);
+      } else {
+        errorMessage = response.body;
+      }
+    } catch (e) {
+      errorMessage = e.toString();
     }
-  } catch (e) {
-    // Handle any errors that occurred during the request
-    developer.log('Error: $e');
+    isLoading = false;
+    notifyListeners();
   }
 }
