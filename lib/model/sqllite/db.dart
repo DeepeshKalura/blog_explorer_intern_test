5dbdfd49bcd4b09519a70a81056e035dc66897e2
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-10-04 22:12:44 +0530

    create a databse

---
+++ lib/model/sqllite/db.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseHelper {
+  static const _databaseName = "blog_database.db";
+  static const _databaseVersion = 1;
+
+  static const table = 'blogs';
+
+  static const columnId = 'id';
+  static const columnImageUrl = 'image_url';
+  static const columnTitle = 'title';
+
+  // Make this a singleton class
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, _databaseName);
+    return await openDatabase(path,
+        version: _databaseVersion, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE $table (
+        $columnId TEXT PRIMARY KEY,
+        $columnImageUrl TEXT,
+        $columnTitle TEXT
+      )
+    ''');
+  }
+
+  Future<int> insert(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    return await db.insert(table, row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAllRows() async {
+    Database db = await instance.database;
+    return await db.query(table);
+  }
+}
